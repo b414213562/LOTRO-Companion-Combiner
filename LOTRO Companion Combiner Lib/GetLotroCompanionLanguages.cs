b6e31efdc3d2643fd99af7b9e120a95673ea13a7
Billy Van Ark <4hchm4xub2@snkmail.com>
2023-12-17 03:28:31 +0100

    Added helper to get which languages are available.

---
+++ LOTRO Companion Combiner Lib/GetLotroCompanionLanguages.cs
@@ -0,0 +1,21 @@
+﻿namespace LOTRO_Companion_Combiner_Lib
+{
+    public class LotroCompanionHelpers
+    {
+        public static List<string> GetLotroCompanionLanguages(string lotroDataPath)
+        {
+            var results = new List<string>();
+
+            var labelsPath = Path.Combine(lotroDataPath, "labels");
+            var labelsDi = new DirectoryInfo(labelsPath);
+
+
+            foreach (var directory in labelsDi.EnumerateDirectories())
+            {
+                results.Add(directory.Name);
+            }
+
+            return results;
+        }
+    }
+}
