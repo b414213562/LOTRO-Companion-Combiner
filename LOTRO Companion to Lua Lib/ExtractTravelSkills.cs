091a0ea18735e6fe533d0dc0ffaf3cd5ab31dc26
Billy Van Ark <4hchm4xub2@snkmail.com>
2023-12-17 03:29:32 +0100

    Added sample project for extracting travel skills to Lua.

---
+++ LOTRO Companion to Lua Lib/ExtractTravelSkills.cs
@@ -0,0 +1,90 @@
+﻿using LOTRO_Companion_Combiner_Lib;
+using NLua;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace LOTRO_Companion_to_Lua_Lib
+{
+    public class ExtractTravelSkills
+    {
+        public ExtractTravelSkills(string lotroDataPath, string langVariable)
+        {
+            // Get list of languages
+            var languages = LotroCompanionHelpers.GetLotroCompanionLanguages(lotroDataPath);
+            var skillsPath = Path.Combine(lotroDataPath, "skills");
+
+            // Open the translated files (ending in -{lang}.xml) and convert them to Lua:
+            foreach (var lang in languages)
+            {
+                var fileName = Path.Combine(skillsPath, $"skills-{lang}.xml");
+                if (File.Exists(fileName))
+                {
+                    var skills = ParseSkillsFile(fileName);
+                    if (skills != null)
+                    {
+                        var outputFileName = Path.Combine(skillsPath, $"skills-{lang}.lua");
+                        var skillsLua = MakeLuaFromXml(skills, lang, langVariable);
+                        File.WriteAllText(outputFileName, skillsLua);
+                    }
+                    else
+                    {
+                        Console.Write($"Couldn't parse {skillsPath}");
+                    }
+                }
+                else
+                {
+                    Console.Write($"Couldn't find {skillsPath}");
+                }
+
+
+            }
+        }
+
+        private string MakeLuaFromXml(skills skills, string lang, string langVariable)
+        {
+            var eol = Environment.NewLine;
+
+            // if (GetClientLanguage() ~= "DE") then return true; end
+
+            var result = 
+                $"if ({langVariable} ~= \"{lang}\") then return; end" + eol +
+                "SKILLS = {" + eol;
+
+            foreach (var skill in skills.Items)
+            {
+                if (skill is skillsSkill s)
+                {                 
+                    if (s.category == "102") // travel skill
+                    {
+                        if (s.identifier == "1879276393") { continue; } // skip Smell the Roses
+
+                        var description = s.description.Replace("\n", " ");
+                        description = description.Replace("\\q", "'");
+
+                        result += $"  [{s.identifier}] = " +
+                            $"{{ " +
+                            $"[\"NAME\"] = \"{s.name}\"; " +
+                            $"[\"DESCRIPTION\"] = \"{description}\" " +
+                            $"[\"EFFECT_ID\"] = \"{s.effect[0].id}\" " +
+                            $"[\"EFFECT_NAME\"] = \"{s.effect[0].name}\" " +
+                            $"}} " + eol;
+                    }
+
+                }
+            }
+
+            result += "};" + eol;
+
+            return result;
+        }
+
+        private static skills? ParseSkillsFile(string fileName)
+        {
+            var serializer = new XmlSerializer(typeof(skills));
+            using var stream = new FileStream(fileName, FileMode.Open);
+            var skills = serializer.Deserialize(stream) as skills;
+            return skills;
+        }
+
+    }
+}
---
+++ LOTRO Companion to Lua/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("LOTRO Companion to Lua:");
+
+if (args.Length == 0)
+{
+    Console.WriteLine("Pass the lotro-data directory as the first argument.");
+    return 1;
+}
+
+var path = args[0];
+var langVariable = "LANGUAGE";
+if (args.Length > 1)
+{
+    langVariable = args[1];
+}
+
+var lib = new LOTRO_Companion_to_Lua_Lib.ExtractTravelSkills(path, langVariable);
+return 0;
